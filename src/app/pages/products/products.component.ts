baab8f8185674fe3a2ea56fed3082143a0bfe36e
Anselme <anselmedupuy@gmail.com>
2025-12-26 15:53:55 +0100

    transfer

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../core/models/product.modele';
+import { ProductService } from '../../core/services/api/product.service';
+
 
 @Component({
   selector: 'app-products',
@@ -7,6 +10,19 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+
+export class ProductsComponent implements OnInit {
+  products : Array<Product> = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+      const url : string = "https://dummyjson.com/products";
+     this.productService.getAllProducts().subscribe({
+       next: (data : {products : Array<Product>}) =>{
+         this.products = data.products;
+       }
+     })
+  }
 
 }
